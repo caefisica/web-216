1d0fd98fd58f447ce79b5ccbab3f8226c1f7e23a
David <20960328+totallynotdavid@users.noreply.github.com>
2026-04-05 16:28:50 -0500

    deps: remove sugar-high (code hightlighter)

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -1,6 +1,5 @@
-import React, { ComponentPropsWithoutRef } from "react";
+import { ComponentPropsWithoutRef } from "react";
 import Link from "next/link";
-import { highlight } from "sugar-high";
 
 type HeadingProps = ComponentPropsWithoutRef<"h1">;
 type ParagraphProps = ComponentPropsWithoutRef<"p">;
@@ -66,10 +65,6 @@ const components = {
         {children}
       </a>
     );
-  },
-  code: ({ children, ...props }: ComponentPropsWithoutRef<"code">) => {
-    const codeHTML = highlight(children as string);
-    return <code dangerouslySetInnerHTML={{ __html: codeHTML }} {...props} />;
   },
   Table: ({ data }: { data: { headers: string[]; rows: string[][] } }) => (
     <table>
