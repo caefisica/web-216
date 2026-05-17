f48fa3acdfac4933daa054640952b9d2a2b1df3e
David <20960328+totallynotdavid@users.noreply.github.com>
2026-05-17 00:16:49 -0500

    test

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -5,5 +5,5 @@ import * as schema from "./schema";
 const connectionString = process.env.DATABASE_URL!;
 
 // For long-running processes (like dev server), use a singleton or pool
-const client = postgres(connectionString);
+const client = postgres(connectionString, { max: 1, idle_timeout: 20, max_lifetime: 60 * 30 });
 export const db = drizzle(client, { schema });
